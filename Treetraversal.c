7486a6d430fadd4298d7b8e561f76471a71db68f
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2023-03-17 06:14:20 +0530

    Update Treetraversal.c

--- Treetraversal.c
+++ Treetraversal.c
@@ -33,20 +33,20 @@ void inorder(struct node *root)
     printf("%d",root->data);
     inorder(root->right);
  }
-void postorder(struct node *root)
- {
-    if(root==NULL)
-     return;
-    printf("%d",root->data); 
-    postorder(root->left);
-    postorder(root->right);
- }
 void preorder(struct node *root)
  {
     if(root==NULL)
      return;
-    preorder(root->left);
-    preorder(root->right);
+    printf("%d",root->data); 
+    pretorder(root->left);
+    pretorder(root->right);
+ }
+void postorder(struct node *root)
+ {
+    if(root==NULL)
+     return;
+    postorder(root->left);
+    postorder(root->right);
     printf("%d",root->data);
  }
 int main()
