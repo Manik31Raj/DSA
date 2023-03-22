da9d2ca6043c30b31da078cce1fcc5efbcbba934
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2023-03-22 14:44:49 +0530

    Update Bst.c

--- Bst.c
+++ Bst.c
@@ -1,207 +1,117 @@
 #include<stdio.h>
-
 #include<malloc.h>
-
- 
-
 struct node{
-
     int data;
-
     struct node* left;
-
     struct node* right;
-
 };
-
- 
-
-struct node* createNode(int data){
-
+struct node* createNode(int data)
+{
     struct node *n; // creating a node pointer
-
     n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
-
     n->data = data; // Setting the data
-
     n->left = NULL; // Setting the left and right children to NULL
-
-    n->right = NULL; // Setting the left and right children to NULL
-
+   n->right = NULL; // Setting the left and right children to NULL
     return n; // Finally returning the created node
-
 }
-
- 
-
-void preOrder(struct  node* root){
-
-    if(root!=NULL){
-
+void preOrder(struct  node* root)
+{
+    if(root!=NULL)
+   {
         printf("%d ", root->data);
-
         preOrder(root->left);
-
         preOrder(root->right);
-
     }
-
 }
-
- 
-
-void postOrder(struct  node* root){
-
-    if(root!=NULL){
-
+void postOrder(struct  node* root)
+{
+    if(root!=NULL)
+     {
         postOrder(root->left);
-
         postOrder(root->right);
-
         printf("%d ", root->data);
-
     }
-
 }
+void inOrder(struct  node* root)
+   {
 
- 
-
-void inOrder(struct  node* root){
-
-    if(root!=NULL){
-
+    if(root!=NULL)
+       {
         inOrder(root->left);
-
         printf("%d ", root->data);
-
         inOrder(root->right);
-
     }
-
 }
-
- 
-
-int isBST(struct  node* root){
-
+int isBST(struct  node* root)
+{
     static struct node *prev = NULL;
-
     if(root!=NULL){
-
-        if(!isBST(root->left)){
-
+        if(!isBST(root->left))
+        {
             return 0;
-
         }
-
-        if(prev!=NULL && root->data <= prev->data){
-
+        if(prev!=NULL && root->data <= prev->data)
+        {
             return 0;
-
         }
-
         prev = root;
-
         return isBST(root->right);
-
     }
-
     else{
-
         return 1;
-
     }
-
 }
-
- 
-
-struct node * searchIter(struct node* root, int key){
-
-    while(root!=NULL){
-
-        if(key == root->data){
-
+struct node * searchIter(struct node* root, int key)
+{
+    while(root!=NULL)
+     {
+        if(key == root->data)
+        {
             return root;
-
         }
-
-        else if(key<root->data){
-
+        else if(key<root->data)
+           {
             root = root->left;
-
-        }
-
+           }
         else{
-
             root = root->right;
-
         }
-
     }
-
     return NULL;
-
 }
-
- 
-
-void insert(struct node *root, int key){
-
+void insert(struct node *root, int key)
+   {
     struct node *prev = NULL;
-
-    while(root!=NULL){
-
+    while(root!=NULL)
+      {
         prev = root;
-
-        if(key==root->data){
-
+        if(key==root->data)
+          {
             printf("Cannot insert %d, already in BST", key);
-
             return;
-
         }
-
-        else if(key<root->data){
-
-            root = root->left;
-
+        else if(key<root->data)
+        {
+           root = root->left;
         }
-
-        else{
-
-            root = root->right;
-
+       else{
+           root = root->right;
         }
-
     }
-
     struct node* new = createNode(key);
-
-    if(key<prev->data){
-
-        prev->left = new;
-
-    }
-
+    if(key<prev->data)
+       {
+        prev->left = new; 
+       }
     else{
-
         prev->right = new;
-
     }
-
 }
-
-struct node *inOrderPredecessor(struct node* root){
-
+Struct node *inOrderPredecessor(struct node* root){
     root = root->left;
-
     while (root->right!=NULL)
-
     {
-
         root = root->right;
-
     }
 
     return root;
