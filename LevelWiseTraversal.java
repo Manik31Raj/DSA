a67107c6d7d73e157db2537979d345e85e5f002a
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-04-15 11:11:10 +0530

    LevelWiseTraversal added (BFS)

---
+++ LevelWiseTraversal.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int key){
+        this.data=key;
+    }
+}
+
+class LevelWiseTraversal            //BFS
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Node root=new Node(1);
+		root.left=new Node(2);
+	    root.right=new Node(3);
+	    root.left.left=new Node(4);
+	    root.left.right=new Node(5);
+	    root.right.left=new Node(6);
+	    root.right.right=new Node(7);
+	    
+	    System.out.println("leve-Wise : "+ levelOrder(root));
+	    
+	    
+	}
+    public static List<List<Integer>> levelOrder(Node root){
+        Queue<Node> queue=new LinkedList<Node>();
+        List<List<Integer>> wrapList=new LinkedList<List<Integer>>();
+        if(root ==  null ) return wrapList;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum=queue.size();
+            List<Integer> subList=new LinkedList<Integer>();
+            for(int i=0;i<levelNum;i++){
+                if(queue.peek().left!=null)  queue.offer(queue.peek().left);
+                if(queue.peek().right!=null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().data);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
