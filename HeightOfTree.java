3dc4ca189bbc9f4a1eb8e4a98093d52374d76c43
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-04-17 12:04:45 +0530

    Create HeightOfTree.java

---
+++ HeightOfTree.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int key){
+        this.data=key;
+    }
+}
+
+class HeightOfTree
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Node root=new Node(1);
+		root.left=new Node(2);
+	    root.right=new Node(3);
+	    root.left.left=new Node(4);
+	    root.left.right=new Node(5);
+	    root.right.left=new Node(6);
+	    root.right.right=new Node(7);
+	    
+	    System.out.println("Depth or Height of Tree is  -> "+maxDepth(root));
+	    
+	}
+	
+	//Uses Recursion and formula as 1+max(left,right)
+    public static int maxDepth(Node root){
+        if(root==null) return 0;
+        
+        int lh=maxDepth(root.left);
+        int rh=maxDepth(root.right);
+        
+        return 1+ Math.max(lh,rh);
+    }
+}
