7a4a83c4f712283f3bd62d2f63931d33b5db0ebf
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-04-19 12:25:40 +0530

    Create TopView.java

---
+++ TopView.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int key){
+        this.data=key;
+    }
+}
+
+class Pair{
+    int x;
+    Node node;
+    public Pair(int x,Node node){
+        this.x=x;
+        this.node=node;
+    }
+}
+
+class TopView          
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Node root=new Node(1);
+		root.left=new Node(2);
+	    root.right=new Node(3);
+	    root.left.left=new Node(4);
+	    root.left.right=new Node(5);
+	    root.right.left=new Node(6);
+	    root.right.right=new Node(7);
+	    
+	    System.out.println("Top-View -> "+topView(root));
+	    
+	}
+	public static List<Integer> topView(Node root){
+	    
+	    if(root == null) return new ArrayList<>();
+	    
+	    TreeMap<Integer,Integer> map=new TreeMap<>();
+	    Queue<Pair> q=new LinkedList<>();
+	    q.offer(new Pair(0,root));
+	    
+	    while(!q.isEmpty()){
+	        Pair p=q.poll();
+	        Node node=p.node;
+	        int z=p.x;
+	        
+	        if(!map.containsKey(z)){
+	            map.put(z,node.data);
+	        }
+	        
+	        if(node.left!=null){
+	            q.offer(new Pair(z-1,node.left));
+	        }
+	        
+	        if(node.right!=null){
+	            q.offer(new Pair(z+1,node.right));
+	        }
+	    }
+	    
+	   return new ArrayList<>(map.values());
+	}
+	
+}
+
