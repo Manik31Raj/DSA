4fbf39df68decde1a38c200cc7954fedfe095090
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-04-20 12:46:41 +0530

    Update GenerateReadme.java

--- GenerateReadme.java
+++ GenerateReadme.java
@@ -1,4 +1,3 @@
-```java
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
@@ -52,4 +51,3 @@ public class GenerateReadme {
         System.out.println("README.md updated!");
     }
 }
-```
