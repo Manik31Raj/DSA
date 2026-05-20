c9a8fd4760e4c9303dc5208f457ff8d2f96da9b5
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-05-20 11:07:12 +0530

    Create DisjointDefination.java

---
+++ GRAPH/DisjointDefination.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class DisjointSet{
+    List<Integer> rank=new ArrayList<>();
+    List<Integer> parent=new ArrayList<>();
+    List<Integer> size=new ArrayList<>();
+    
+    public DisjointSet(int n){
+        for(int i=0;i<n+1;i++){
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    public int findUltimateParent(int node){
+        
+        if(node==parent.get(node)){
+            return node;
+        }
+        
+        int ultParent=findUltimateParent(parent.get(node));
+        parent.set(node,ultParent);
+        return parent.get(node);
+    }
+    
+    
+    public void unionByRank(int u,int v){
+        
+        int ultParent_u=findUltimateParent(u);
+        int ultParent_v=findUltimateParent(v);
+        
+        if(ultParent_u == ultParent_v) return;
+        
+        if(rank.get(ultParent_u) < rank.get(ultParent_v)){
+            parent.set(ultParent_u,ultParent_v);
+        }else if(rank.get(ultParent_u) > rank.get(ultParent_v)){
+            parent.set(ultParent_v,ultParent_u);
+        }else{
+            parent.set(ultParent_u,ultParent_v);
+            int rankV=rank.get(ultParent_v);
+            rank.set(ultParent_v,rankV+1);
+        }
+    }
+    
+    public void unionBySize(int u,int v){
+        
+        int ultParent_u=findUltimateParent(u);
+        int ultParent_v=findUltimateParent(v);
+        
+        if(ultParent_u == ultParent_v) return;
+        
+        if(size.get(ultParent_u) < size.get(ultParent_v)){
+            parent.set(ultParent_u,ultParent_v);
+            size.set(ultParent_v,size.get(ultParent_u)+size.get(ultParent_v));
+        }else{
+            parent.set(ultParent_v,ultParent_u);
+            size.set(ultParent_u,size.get(ultParent_u)+size.get(ultParent_v));
+        }
+    }
+    
+    public int findRank(int node){
+        return rank.get(node);
+    }
+    
+    public void print(){
+        System.out.println("Rank    -> "+rank);
+        System.out.println("Parent  -> "+parent);
+        System.out.println("Size    -> "+size);
+    }
+}
+
+class DisjointDefination
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    DisjointSet ds=new DisjointSet(7);
+	    int[][] edges={ {1,2},{2,3},{4,5},{6,7},{5,6},{3,7}};
+	    for(int i=0;i<edges.length;i++){
+	        //ds.unionByRank(edges[i][0],edges[i][1]);
+	        ds.unionBySize(edges[i][0],edges[i][1]);
+	    }
+	    ds.print();
+	}
+}
