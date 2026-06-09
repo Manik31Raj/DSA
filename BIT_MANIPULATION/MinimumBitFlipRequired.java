dc99dafeedf4068414a7ccfed77066cdacd15959
Manik Raj Chaurasia <108018610+Manik31Raj@users.noreply.github.com>
2026-06-09 11:29:17 +0530

    Create MinimumBitFlipRequired.java

---
+++ BIT_MANIPULATION/MinimumBitFlipRequired.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MinimumBitFlipRequired
+{
+	public static void main (String[] args)
+	{
+	    int start=10,goal=7;
+		System.out.println("Minimum Bit flip required to convert "+start+" to "+goal+" -> " +minBitFlip(3,4));
+	}
+	public static int minBitFlip(int start,int goal){
+	    
+	    int num=start^goal;
+	    
+	    int count=0;
+	    while(num!=0){
+	        num= num & (num-1);
+	        count++;
+	    }
+	    
+	    return count;
+	}
+   
+   /*	
+	public static int minBitFlips(int start,int goal){
+	    
+	    int num=start^goal;
+	    
+	    int count=0;
+	    while(num>0){
+	       int x=num%2;
+	       if(x==1) count++;
+	       num=num/2;
+	    }
+	    
+	    return count;
+	}
+	*/
+}
